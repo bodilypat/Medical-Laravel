1aed30b2c1bc43525c879d1cf77ffe12c3be005c
bodilypat <139784676+bodilypat@users.noreply.github.com>
2026-03-16 13:15:39 -0700

    Add files via upload

---
+++ Backend/database/migrations/create_users_table.php
@@ -0,0 +1,40 @@
+<!-- database/migrations/0000_00_00_000000_create_users_table.php -->
+
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('users', function (Blueprint $table) {
+            $table->id('user_id');
+            $table->string('username')->unique();
+            $table->string('email')->unique();
+            $table->boolean('email_verified')->default(false);
+            $table->string('password');
+            $table->enum('role', ['admin', 'doctor', 'patient', 'staff']);
+            $table->string('remember_token')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('users');
+    }
+}
+

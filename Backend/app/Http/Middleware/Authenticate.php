aaee140b06431067d359b63b6ff26092720d8ba3
bodilypat <139784676+bodilypat@users.noreply.github.com>
2026-03-16 12:18:45 -0700

    Create Authenticate.php

---
+++ Backend/app/Http/Middleware/Authenticate.php
@@ -0,0 +1,29 @@
+<!-- //app/Http/Middleware/Authenticate.php -->
+
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+class Authenticate
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle(Request $request, Closure $next)
+    {
+        if (!Auth::check()) {
+            return redirect()->route('login')->with('error', 'Please login to access this page.');
+        }
+
+        return $next($request);
+    }
+}
+
+

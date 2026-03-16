e2836cd833345a6d3e148423af128f457f6a8c5b
bodilypat <139784676+bodilypat@users.noreply.github.com>
2026-03-16 09:14:46 -0700

    Create create_patients_table.php

---
+++ Backend/database/migrations/create_patients_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePatientsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('patients', function (Blueprint $table) {
+            $table->id('patient_id');
+            $table->string('first_name', 50);
+            $table->string('last_name', 50);
+            $table->enum('gender', ['male', 'female', 'other']);
+            $table->date('date_of_birth')->nullable();
+            $table->string('phone', 15)->nullable();
+            $table->string('email', 100)->nullable();
+            $table->text('address')->nullable();
+            $table->enum('blood_group', ['A+', 'A-', 'B+', 'B-', 'AB+', 'AB-', 'O+', 'O-'])->nullable();
+            $table->timestamp('registered_at')->useCurrent();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('patients');
+    }
+}
+
